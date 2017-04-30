30f3a5d4812ba6a5c5e2e6b3fcb8ecc0b9761ade
王导 <elementuse@live.cn>
2017-04-30 14:09:10 +0800

    init

---
+++ WPTabBarcontroller.framework/Headers/WPTabBarController.h
@@ -0,0 +1,16 @@
+//
+//  WPTabBarcontroller.h
+//  WPTabBarcontroller
+//
+//  Created by tony on 17/4/28.
+//  Copyright © 2017年 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPTabBarController : UITabBarController
+
+- (id)initWithStartImage:(NSString*)startImage itemImages:(NSArray *)itemImages atPoint:(CGPoint)point;
+
+@end
+
